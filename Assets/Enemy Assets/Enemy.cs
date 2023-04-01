d9fd84ffa48f044c73890d9d9f295ba8239371f4
Jakubyte <jakubdymar@gmail.com>
2023-04-01 19:17:43 +0100

    Refactored Enemy

--- Assets/Enemy Assets/Enemy.cs
+++ Assets/Enemy Assets/Enemy.cs
@@ -2,22 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[SerializeField]
 public class Enemy : MonoBehaviour
 {
-    private int currPos;
-    private List<Vector3> path;
-    public void init(List<Vector3> path)
-    {
-        this.path = path;
-        this.currPos = 1;
-        InvokeRepeating("move", 1f, 1f);
+    public float health;
+    public float speed;
+
+    void _init_(ref EnemyScriptableObject _stats, ref List<Vector3> _path) {
+        path = _path;
+        stats = _stats;
+        health = this.stats.hp;
+        speed = this.stats.speed;
+
+        pathIndex = 0;
+        initialised = true;
     }
 
-    void move()
-    {
-        if (currPos == path.Count - 1) Destroy(this.gameObject);
-        this.transform.position = this.path[this.currPos] + Vector3.one * 0.5f;
-        // yield return new WaitForSecondsRealtime(1);
-        this.currPos++;
+    private void move() {
+        if (pathIndex == path.Count - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // TODO: Terrain offset by 1/2 in every direction, fix in implementation
+        gameObject.transform.position = path[pathIndex++];
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // make sure object has been initialised
+        if (!initialised) {
+            Debug.Log("Please initialise object with _init_() call");
+            return;
+        }
+
+        InvokeRepeating("move", 0f, speed);
+    }
+
+    public void takeDamage(float damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // returns the number of elements left to traverse in the path
+    public int distanceLeftToTravel() {
+        return ((path.Count - 1) - pathIndex);
+    }
+
+    // private members
+    private EnemyScriptableObject stats;
+    private List<Vector3> path;
+    private int pathIndex;
+    private bool initialised = false;   // used to determine if _init_() has been called and member fields have been set
 }
